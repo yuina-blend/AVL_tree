185d616038752a8083279d86fcda06217981b92f
yuina-blend <moon.h14.dd2.as@gmail.com>
2021-06-21 14:35:43 +0900

    typedefを削除

--- binary_tree.h
+++ binary_tree.h
@@ -1,6 +1,9 @@
+#ifndef BINARY_TREE
+#define BINARY_TREE
 //二分木の構造定義
-typedef struct binary_tree_node {
+struct binary_tree_node {
     int value;
     struct binary_tree *right;
     struct binary_tree *left;
-} node;
+};
+#endif
